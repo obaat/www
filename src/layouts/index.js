9ad420d2ca37373c78198fa266e8d82fcf013acb
Simon Elliott <simon@papercreatures.com>
2017-07-31 17:18:24 +0300

    remove default css

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,8 +5,6 @@ import Helmet from 'react-helmet'
 import Header from '../components/Header';
 import {ThemeProvider} from 'glamorous';
 
-import './index.css'
-
 const theme = {
   colors: {
     blue: '#07c',
@@ -26,6 +24,8 @@ const TemplateWrapper = ({
       >
 
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.0/css/bulma.min.css"/>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
       </Helmet>
       <Header />
       <ThemeProvider theme={ theme }>
