28d34c56ebf4c75df24730f0d6f0d00d738b7906
Simon Elliott <simon@papercreatures.com>
2017-08-04 16:42:22 +0300

    contentful + glamorous plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `One Brick at a Time`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `kpfz2fzy0si4`,
+        accessToken: `39591a0146faa8a21af1e25c47661572b42f506cb5a7c3402bf7d6acf1bf9d87`,
+      },
+    }, `gatsby-transformer-remark`, `gatsby-plugin-react-helmet`, `gatsby-plugin-glamorous`
+  ]
 }
