f3a023e591e1b572c0e46c9934c0bdeb9ffa2a14
Simon Elliott <simon@papercreatures.com>
2017-08-04 17:30:29 +0300

    put css in html not component

--- src/html.js
+++ src/html.js
@@ -45,6 +45,10 @@ class Html extends Component {
           />
           <title>One Brick at a Time</title>
           <TypographyStyle typography={typography} />
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
+          <link rel="stylesheet" href="https://unpkg.com/tachyons-box-sizing@3.1.7/css/tachyons-box-sizing.min.css" />
           {css}
         </head>
         <body>
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -26,10 +26,6 @@ const TemplateWrapper = ({ children }) =>
           { name: 'keywords', content: 'obaat, uganda' },
         ]}
       >
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
-        <link rel="stylesheet" href="https://unpkg.com/tachyons-box-sizing@3.1.7/css/tachyons-box-sizing.min.css" />
       </Helmet>
       {children()}
       <Header />
