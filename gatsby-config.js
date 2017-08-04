8e844f0c3af9d31fe237697eb15e9bfce6be019f
Simon Elliott <simon@papercreatures.com>
2017-08-04 21:19:59 +0300

    use working plugin for pre-rendering css

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,6 @@ module.exports = {
         spaceId: `kpfz2fzy0si4`,
         accessToken: `39591a0146faa8a21af1e25c47661572b42f506cb5a7c3402bf7d6acf1bf9d87`,
       },
-    }, `gatsby-transformer-remark`, `gatsby-plugin-react-helmet`, `gatsby-plugin-glamorous`
+    }, `gatsby-transformer-remark`, `gatsby-plugin-react-helmet`, `gatsby-plugin-glamor`
   ]
 }
--- src/html.js
+++ src/html.js
@@ -3,15 +3,6 @@ import * as PropTypes from "prop-types"
 import { TypographyStyle, GoogleFont } from "react-typography"
 import typography from "./utils/typography"
 
-let stylesStr
-if (process.env.NODE_ENV === `production`) {
-  try {
-    stylesStr = require(`!raw-loader!../public/styles.css`)
-  } catch (e) {
-    console.log(e)
-  }
-}
-
 const propTypes = {
   headComponents: PropTypes.node.isRequired,
   body: PropTypes.node.isRequired,
@@ -20,18 +11,9 @@ const propTypes = {
 
 class Html extends Component {
   render() {
-    let css
-    if (process.env.NODE_ENV === `production`) {
-      css = (
-        <style
-          id="gatsby-inlined-css"
-          dangerouslySetInnerHTML={{ __html: stylesStr }}
-        />
-      )
-    }
 
     return (
-      <html op="news" lang="en">
+      <html op="charity" lang="en">
         <head>
           <title>One Brick at a Time</title>
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
@@ -40,12 +22,11 @@ class Html extends Component {
           <link rel="stylesheet" href="https://unpkg.com/tachyons-box-sizing@3.1.7/css/tachyons-box-sizing.min.css" />
           <TypographyStyle typography={typography} />
           <GoogleFont typography={typography} />
-          {css}
           {this.props.headComponents}
 
           <meta name="referrer" content="origin" />
           <meta charSet="utf-8" />
-          <meta name="description" content="Gatsby example site using Glamor" />
+          <meta name="description" content="" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
@@ -65,5 +46,4 @@ class Html extends Component {
 }
 
 Html.propTypes = propTypes
-
 module.exports = Html
