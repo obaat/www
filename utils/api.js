34a0d93a7c41265836dd3569ba079b022e71d236
Simon Elliott <simon@papercreatures.com>
2017-10-06 00:28:48 +0300

    memoize for 30 mins

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,5 @@
 const Prismic = require("prismic-javascript")
-const memoize = require("lodash/memoize")
+const memoizee = require("memoizee")
 
 const types = {
   VOLUNTEERING: "volunteer_opportunity",
@@ -29,10 +29,13 @@ const types = {
 // const accessToken =
 //   "MC5XWWhTS2lZQUFOLUtDUFZy.77-977-9MzHvv73vv73vv71L77-9YUnvv73vv70_77-9LSF9Le-_vR3vv73vv70lUihUNSrvv73vv73vv70"
 
-const apiBuilder = () =>
-  Prismic.getApi("https://one-brick.cdn.prismic.io/api/v2", {
-    // accessToken,
-  })
+const apiBuilder = memoizee(
+  () =>
+    Prismic.getApi("https://one-brick.cdn.prismic.io/api/v2", {
+      // accessToken,
+    }),
+  { maxAge: 60 * 30 },
+)
 
 const getSingleton = async type => {
   const api = await apiBuilder()
