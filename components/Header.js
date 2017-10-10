057388261ff1ff435c9ea015e503a8d2d7eee762
Simon Elliott <simon@papercreatures.com>
2017-10-10 18:59:14 +0300

    change logo size

--- components/Header.js
+++ components/Header.js
@@ -118,7 +118,7 @@ export default class Header extends Component {
             <Link to="/">
               <Logo
                 color={scrolled ? theme.colors.base[0] : "#fff"}
-                size={16}
+                size={20}
               />
             </Link>
             <Menu volunteering={this.props.volunteering} scrolled={scrolled} />
