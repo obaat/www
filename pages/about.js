ea730242cb36c5625efb963a98ea06554275d105
Simon Elliott <simon@papercreatures.com>
2017-10-10 23:28:38 +0300

    mobile friendly about us

--- pages/about.js
+++ pages/about.js
@@ -13,10 +13,15 @@ const About = ({ content = {} }) => {
         {content.section.map((section, i) => (
           <Border bottom borderColor="gray4" key={i} mb={4}>
             <Flex>
-              <Box w={0.45} mr={i % 2 ? 4 : 0} ml={i % 4 ? 0 : 4} mt={50}>
+              <Box
+                w={[1, 1, 0.45, 0.45]}
+                mr={i % 2 ? 4 : 0}
+                ml={i % 4 ? 0 : 4}
+                mt={50}
+              >
                 <BackgroundImage ratio={2 / 3} src={section.image.url} />
               </Box>
-              <Box w={0.55} order={i % 2 ? 1 : -1} mb={2}>
+              <Box w={[1, 1, 0.55, 0.55]} order={i % 2 ? 1 : -1} mb={2}>
                 <PrismicRichText source={section.title} forceType="heading6" />
                 <PrismicRichText source={section.description} />
               </Box>
