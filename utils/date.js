9ec8f0f02e596d3db8ee3fa53fcfbc9b0e9259c0
Simon Elliott <simon@papercreatures.com>
2017-11-02 09:45:23 +0300

    fix dates in safari

--- utils/date.js
+++ utils/date.js
@@ -1,9 +1,10 @@
 import React from "react"
 import format from "date-fns/format"
+import parse from "date-fns/parse"
 
 const ourFormat = "MMMM YYYY"
 
 export const HumanDate = ({ iso }) => {
-  const dt = Date.parse(iso)
+  const dt = parse(iso)
   return <span>{format(dt, ourFormat)}</span>
 }
