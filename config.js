cf0a72ef5ee20a0773ae6876f77af85a981007db
Simon Elliott <simon@papercreatures.com>
2018-04-21 18:42:36 +0300

    update node

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 // read process.env.NODE_ENV to set values depending on environment
 
-export const ANALYTICS_TRACKING_ID = process.env.ANALYTICS_TRACKING_ID || "FIXME";
+module.exports = {
+  ANALYTICS_TRACKING_ID: process.env.ANALYTICS_TRACKING_ID || "FIXME",
+}
